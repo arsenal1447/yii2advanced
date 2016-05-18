3f8fd63ff89e605ca8f81ae6d060c708462f5b9a
xinxin <xinxin@rrkd.cn>
2016-05-18 18:37:35 +0800

    1,后台修改搜索功能

--- backend/models/PostSearch.php
+++ backend/models/PostSearch.php
@@ -22,7 +22,7 @@ class PostSearch extends Post
     {
         return [
             [['post_id', 'post_meta_id', 'post_user_id', 'post_view_count', 'post_reply_count', 'post_favorite_count', 'post_like_count', 'post_thanks_count', 'post_hate_count', 'post_status', 'post_order', 'post_create', 'post_update'], 'integer'],
-            [['post_type', 'post_title', 'post_author', 'excerpt', 'image', 'post_content', 'post_tags', 'category_name', 'username'], 'safe'],//safe表示安全字段,不想被验证
+            [['post_type', 'post_title', 'post_author', 'post_excerpt', 'post_image', 'post_content', 'post_tags', 'category_name', 'username'], 'safe'],//safe表示安全字段,不想被验证
         ];
     }
 
@@ -68,30 +68,38 @@ class PostSearch extends Post
         }
 
         $query->andFilterWhere([
-            'post_id' => $this->id,
+            'post_id' => $this->post_id,
             'post_meta_id' => $this->post_meta_id,
-            'post_user_id' => $this->user_id,
-            'post_view_count' => $this->view_count,
-            'post_reply_count' => $this->comment_count,
-            'post_favorite_count' => $this->favorite_count,
-            'post_like_count' => $this->like_count,
-            'post_thanks_count' => $this->thanks_count,
-            'post_hate_count' => $this->hate_count,
-            Post::tableName() . '.post_status' => $this->status,
-            'post_order' => $this->order,
-            'post_create' => $this->created_at,
-            'post_update' => $this->updated_at,
+            'post_user_id' => $this->post_user_id,
+            'post_view_count' => $this->post_view_count,
+            'post_reply_count' => $this->post_reply_count,
+            'post_favorite_count' => $this->post_favorite_count,
+            'post_like_count' => $this->post_like_count,
+            'post_thanks_count' => $this->post_thanks_count,
+            'post_hate_count' => $this->post_hate_count,
+            Post::tableName() . '.post_status' => $this->post_status,
+            'post_order' => $this->post_order,
+//             'post_create' => $this->post_create,
+//             'post_updates' => $this->post_update,
         ]);
+        
+        
+        if($this->post_update!=""){
+            $createdAt = strtotime($this->post_update);
+            $createdAtEnd = $createdAt + 24*3600;
+            $query->andFilterWhere(['between', 'post_updates', $createdAt,$createdAtEnd]);
+        }
+        
 
-        $query->andFilterWhere(['like', Post::tableName() . '.post_type', $this->type])
-            ->andFilterWhere(['like', 'post_title', $this->title])
-            ->andFilterWhere(['like', 'post_author', $this->author])
-            ->andFilterWhere(['like', 'post_excerpt', $this->excerpt])
-            ->andFilterWhere(['like', 'post_image', $this->image])
-            ->andFilterWhere(['like', 'post_content', $this->content])
-            ->andFilterWhere(['like', 'post_tags', $this->tags])
-            ->andFilterWhere(['like', 'post_name', $this->category_name])
-            ->andFilterWhere(['like', 'post_user_name', $this->username]);
+        $query->andFilterWhere(['like', Post::tableName() . '.post_type', $this->post_type])
+            ->andFilterWhere(['like', 'post_title', $this->post_title])
+            ->andFilterWhere(['like', 'post_author', $this->post_author])
+            ->andFilterWhere(['like', 'post_excerpt', $this->post_excerpt])
+            ->andFilterWhere(['like', 'post_image', $this->post_image])
+            ->andFilterWhere(['like', 'post_content', $this->post_content])
+            ->andFilterWhere(['like', 'post_tags', $this->post_tags])
+            ->andFilterWhere(['like', 'cat_name', $this->category_name])
+            ->andFilterWhere(['like', 'user_name', $this->username]);
 
         return $dataProvider;
     }
